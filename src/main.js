67006d114b8b303581bfe093f66306eee5f0e40e
Daryl Sew <darylsew@gmail.com>
2014-06-29 05:33:16 -0700

    Basic layout

--- src/main.js
+++ src/main.js
@@ -1,27 +1,80 @@
-define(function(require, exports, module) {
-    // import dependencies
-    var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
+/**
+ * Copyright (c) 2014 Famous Industries, Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE.
+ *
+ * @license MIT
+ */
 
-    // create the main context
+/**
+ * HeaderFooterLayout
+ * ------------------
+ *
+ * HeaderFooterLayout is a layout which will arrange three renderables
+ * into a header and footer area of defined size and a content area
+ * of flexible size.
+ *
+ * In this example we create a basic HeaderFooterLayout and define a 
+ * size for the header and footer
+ */
+
+define(function(require, exports, module) {
+    var Engine             = require("famous/core/Engine");
+    var Surface            = require("famous/core/Surface");
+    var HeaderFooterLayout = require("famous/views/HeaderFooterLayout");
+
     var mainContext = Engine.createContext();
 
-    // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: 'http://code.famo.us/assets/famous_logo.svg',
-        classes: ['double-sided']
+    var layout = new HeaderFooterLayout({
+        headerSize: 100,
+        footerSize: 50
     });
 
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function(){
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
+    layout.header.add(new Surface({
+        size: [undefined, 100],
+        content: "Header",
+        classes: ["red-bg"],
+        properties: {
+            lineHeight: "100px",
+            textAlign: "center"
         }
-    });
+    }));
 
-    mainContext.add(centerSpinModifier).add(logo);
+    layout.content.add(new Surface({
+        size: [undefined, undefined],
+        content: "Content",
+        classes: ["grey-bg"],
+        properties: {
+            lineHeight: window.innerHeight - 150 + 'px',
+            textAlign: "center"
+        }
+    }));
+
+    layout.footer.add(new Surface({
+        size: [undefined, 50],
+        content: "Footer",
+        classes: ["red-bg"],
+        properties: {
+            lineHeight: "50px",
+            textAlign: "center"
+        }
+    }));
+
+    mainContext.add(layout);
 });
